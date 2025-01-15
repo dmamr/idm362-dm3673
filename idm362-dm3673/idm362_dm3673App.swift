6e18a335de2e46fd30608b52931291f11a2378d7
Riya M <mamratovadariya@gmail.com>
2025-01-14 20:00:58 -0500

    Initial Commit

---
+++ idm362-dm3673/idm362_dm3673App.swift
@@ -0,0 +1,17 @@
+//
+//  idm362_dm3673App.swift
+//  idm362-dm3673
+//
+//  Created by Dariya Mamratova on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct idm362_dm3673App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
